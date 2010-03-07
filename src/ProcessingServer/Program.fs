130219401c7d9c46883132b6266d04b29c03f2e3
Mike Chaliy <mike@chaliy.name>
2010-03-07 21:40:50 +0200

    Added processing agent

---
+++ src/ProcessingServer/Program.fs
@@ -0,0 +1,58 @@
+﻿open Shared
+open System.Threading
+
+type Task = {
+    ID : ID
+    Handler : string
+}
+
+module TaskStorage =
+    let TASK1_ID = "TASK1_ID"  
+    let last() = 
+        { ID = TASK1_ID 
+          Handler = "Example" }
+
+type ProcessingAgent() =    
+       
+    let started = new Event<ID>()
+    let success = new Event<ID>()
+    let failed = new Event<ID * System.Exception>()
+
+    let resolveHadler t = fun unit -> Thread.Sleep(1000); ()
+
+    let syncContext = SynchronizationContext.CaptureCurrent()  
+    let raise event args = syncContext.RaiseEvent event args
+
+    let agent = 
+        new Agent<Task>(fun inbox ->
+                            async {
+                                while true do
+                                    let! task = inbox.Receive()                                   
+                                    let handler = resolveHadler task
+
+                                    raise started task.ID                                    
+                                    try                             
+                                        handler()
+                                        raise success task.ID
+                                    with
+                                    | x -> raise failed (task.ID, x)
+                            } )    
+
+    member x.Post msg = agent.Post msg
+    member x.Start () = agent.Start()
+    member x.Started = started.Publish
+    member x.Success = success.Publish
+
+let agent = new ProcessingAgent()
+agent.Started.Add(printfn "Started %s")
+agent.Success.Add(printfn "Success %s")
+
+agent.Start()
+
+agent.Post { ID = "TASK1_ID"
+             Handler = "Example" }
+
+agent.Post { ID = "TASK2_ID"
+             Handler = "Example #2" }
+
+System.Console.ReadLine() |> ignore
---
+++ src/ProcessingServer/Shared.fs
@@ -0,0 +1,21 @@
+﻿module Shared
+
+open System.Threading
+ 
+type ID = string
+type Agent<'T> = MailboxProcessor<'T>
+
+type SynchronizationContext with 
+
+    /// A standard helper extension method to raise an event on the GUI thread
+    member syncContext.RaiseEvent (event: Event<_>) args = 
+        //let mutable syncContext : SynchronizationContext = null
+        syncContext.Post((fun _ -> event.Trigger args), state = null)
+
+ 
+    /// A standard helper extension method to capture the current synchronization context.
+    /// If none is present, use a context that executes work in the thread pool.
+    static member CaptureCurrent () = 
+        match SynchronizationContext.Current with 
+        | null -> new SynchronizationContext()
+        | ctxt -> ctxt
