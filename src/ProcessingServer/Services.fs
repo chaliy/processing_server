a91f40e072349ce935e50ce2f38f74b595497709
Mike Chaliy <mike@chaliy.name>
2010-03-09 15:49:48 +0200

    WCF endpoint spec now ensure that posted task is correct

--- src/ProcessingServer/Services.fs
+++ src/ProcessingServer/Services.fs
@@ -13,27 +13,27 @@ open Model
 // *************
 
 [<DataContract(Namespace="urn:org:mir:processing-v1.0")>]
-type DataItem() =
-    [<DataMember(Name="Key", IsRequired = true, Order = 0)>]
-    let mutable key : string = ""
-    [<DataMember(Name="Value", IsRequired = true, Order = 1)>]
+type DataItem() =    
+    let mutable key : string = ""    
     let mutable arg_value : string = ""
     
-    member public x.Key with get() = key
-    member public x.Value with get() = arg_value    
+    [<DataMember(Name="Key", IsRequired = true, Order = 0)>]
+    member x.Key with get() = key and set(v) = key <- v
+    [<DataMember(Name="Value", IsRequired = true, Order = 1)>]
+    member x.Value with get() = arg_value and set(v) = arg_value <- v   
 
 [<DataContract(Namespace="urn:org:mir:processing-v1.0")>]
-type Task() =  
-    [<DataMember(Name = "ID", IsRequired = true, Order = 0)>]  
+type Task() =      
     let mutable id : string = ""
-    [<DataMember(Name = "Handler", IsRequired = true, Order = 1)>]
-    let mutable handler : string = ""
-    [<DataMember(Name = "Data", IsRequired = true, Order = 2)>]
+    let mutable handler : string = ""    
     let mutable data : List<DataItem> = List<DataItem>()
 
-    member public x.ID with get() = id
-    member public x.Handler with get() = handler
-    member public x.Data with get() = data
+    [<DataMember(Name = "ID", IsRequired = true, Order = 0)>]
+    member x.ID with get() = id and set(v) = id <- v
+    [<DataMember(Name = "Handler", IsRequired = true, Order = 1)>]
+    member x.Handler with get() = handler and set(v) = handler <- v
+    [<DataMember(Name = "Data", IsRequired = true, Order = 2)>]
+    member x.Data with get() = data and set(v) = data <- v    
 
 [<ServiceContract(Namespace = "urn:org:mir:processing-v1.0")>]
 type TaskProcessing =    
--- src/Tests/Services.fs
+++ src/Tests/Services.fs
@@ -2,7 +2,9 @@
 
 open FsSpec
 open System
+open System.Collections.Generic
 open System.ServiceModel
+open System.Threading
         
 open Shared
 open Services
@@ -18,12 +20,43 @@ module ``Describe service agent`` =
     let agent = new ServiceAgent()
     agent.Start()
 
+    // TODO Move this to FsSpec
+    type Waiter<'T>() =
+        let mutable result = None
+        let resEvent = new ManualResetEvent(false)
+        let sync = new obj()
+                
+        member x.Result() : 'T =
+                match result with
+                | Some res -> res
+                | None -> failwith "Result not ready"
+
+        member x.RegisterResult (res:'T) =                
+                    lock sync (fun () -> 
+                        result <- Some(res)
+                        resEvent.Set () |> ignore )
+
+        member x.Wait() = resEvent.WaitOne() |> ignore
+
+    
     let ``should listen for tasks`` = spec {
-        //use posted = agent.Posted.Subscribe()
+        
+        let waiter  = Waiter<Model.Task>()
+
+        // TODO replace let x with use x when FsSpec will support this
+        let x = agent.Posted.Subscribe(waiter.RegisterResult)        
         let client = createClient()
+
         let task = new Task()
-//        task.ID <- "NewID"
-//        task.Handler <- "Example"
-//        task.Data <- new System.Collections.List<DataItem>()
+        task.ID <- "NewID"
+        task.Handler <- "Example"
+        task.Data <- new List<DataItem>()
         client.Post(task)
-    }
+
+        waiter.Wait()
+
+        waiter.Result().should_not_be_null
+        waiter.Result().ID.should_be_equal_to("NewID")
+                
+        x.Dispose()
+    }    
