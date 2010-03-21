0e460bb56944dae68136d7fc6196401bf0d71782
Mike Chaliy <mike@chaliy.name>
2010-03-21 18:43:52 +0200

    Added support for situations when there is no handlers found

--- src/ProcessingServer/Processing.fs
+++ src/ProcessingServer/Processing.fs
@@ -32,35 +32,54 @@ type ProcessingAgent(storage : TaskStorage,
         { new IHandlerContext with
             member x.Data = t.Data
             member x.Trace(msg) = trace(msg) }
+
+    let handleFailure id ex =
+        trace (sprintf "ProcessingAgent: Task failed %s" id)
+        lock statsLock (fun () -> runnig <- runnig - 1 ) // Stats
+        storage.MarkFailed id ex
+        pingTrottler.Ping()
+        
+
+    let resolveHandler task ctx = 
+        let applicable = handlers 
+                         |> List.filter(fun h -> h.CanHandle(ctx)) 
+
+        match applicable.Length with
+        | 0 -> 
+            handleFailure task.ID (System.Exception("Handler was not found.")) 
+            None  
+        | 1 -> Some(applicable |> Seq.head)
+        | _ -> 
+            trace (sprintf "ProcessingAgent: More then one handler found for task %s. Executing first." task.ID)
+            Some(applicable |> Seq.head)
     
     let wrap task =                        
         let ctx = createContext task
-        let handler = handlers 
-                      |> List.find(fun h -> h.CanHandle(ctx))
+        let handler = resolveHandler task ctx        
 
-        thread (fun () -> 
-                let id = task.ID
-                
-                trace (sprintf "ProcessingAgent: Task started %s" id)
-                lock statsLock (fun () -> runnig <- runnig + 1 ) // Stats
-                storage.MarkStarted id
-                let sw = new Stopwatch()
-                sw.Start()
-                                
-                try
-                    handler.Handle(ctx)
-                    
-                    sw.Stop() |> ignore
-                    trace (sprintf "ProcessingAgent: Task success %s (Time: %i ms)" id sw.ElapsedMilliseconds)
-                    lock statsLock (fun () -> runnig <- runnig - 1 ) // Stats
-                    storage.MarkSuccess id
-                    pingTrottler.Ping()
-                with
-                | x -> 
-                    trace (sprintf "ProcessingAgent: Task failed %s" id)
-                    lock statsLock (fun () -> runnig <- runnig - 1 ) // Stats
-                    storage.MarkFailed id x                            
-                    pingTrottler.Ping() )
+        match handler with
+        | Some h ->
+                Some(new Executable(new System.Action(fun () -> 
+
+                        let id = task.ID                                    
+                        trace (sprintf "ProcessingAgent: Task started %s" id)
+                        lock statsLock (fun () -> runnig <- runnig + 1 ) // Stats
+                        storage.MarkStarted id
+                        let sw = new Stopwatch()
+                        sw.Start()
+                                        
+                        try
+                            h.Handle(ctx)
+                            
+                            sw.Stop() |> ignore
+                            trace (sprintf "ProcessingAgent: Task success %s (Time: %i ms)" id sw.ElapsedMilliseconds)
+                            lock statsLock (fun () -> runnig <- runnig - 1 ) // Stats
+                            storage.MarkSuccess id
+                            pingTrottler.Ping()
+                        with
+                        | x -> handleFailure task.ID x                            
+                    ), System.Threading.Tasks.TaskCreationOptions.PreferFairness )  )
+        | None -> None        
 
     let pickTasks() =
         trace "ProcessingAgent: Ping"
@@ -69,10 +88,12 @@ type ProcessingAgent(storage : TaskStorage,
             trace (sprintf "ProcessingAgent: Tasks recieved %i" tasks.Length)            
                   
             tasks 
-            |> Seq.map(wrap)
-            |> Seq.iter(fun t -> 
-                            all.Add(t)
-                            t.Start() )
+            |> Seq.map(wrap)            
+            |> Seq.iter(fun t -> match t with
+                                 | Some t -> 
+                                            all.Add(t)
+                                            t.Start() 
+                                 | None -> ())
 
     let start() =
         pingTrottler.Pushed.Add(fun _ -> pickTasks())
